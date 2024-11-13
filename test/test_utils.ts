a6408348b21a51952a54759a17d91dad27b8de70
Michael Antonio <michaeljantonio@msn.com>
2024-11-12 21:34:55 -0800

    feat:  Finish xforms to normalize snapshots.
    Saves me from having to review snapshot changes every checking.  Now snapshots
    won't change as often.

--- test/test_utils.ts
+++ test/test_utils.ts
@@ -3,8 +3,12 @@ import { CfJsonType, StringReplacer, Transform } from '@michanto/cdk-orchestrati
 import { Stack } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
+/**
+ * EnsureChangeInStack logicalId changes based on the BUILD_TIME constant.
+ * Freeze it to a constant value.
+ */
 export class NormalizeEnsureChangeInStack extends Transform {
-  constructor(scope: Construct, id: string) {
+  constructor(scope: Construct, id: string = 'NormalizeEnsureChangeInStack') {
     super(scope, id);
   }
 
@@ -22,13 +26,49 @@ export class NormalizeEnsureChangeInStack extends Transform {
   }
 }
 
+/**
+ * S3Key changes every time library code changes, including library updates.
+ *
+ * Freeze it to a constant value.
+ */
+export class NormalizeCodeS3Key extends Transform {
+  constructor(scope: Construct, id: string = 'NormalizeCodeS3Key') {
+    super(scope, id);
+  }
+
+  apply(template: CfJsonType): CfJsonType {
+
+    if (template.Resources.CDKORCHCUSTOMRESOURCEResourcesCDKORCHCUSTOMRESOURCEOnEventB60C9E76) {
+      template.Resources.CDKORCHCUSTOMRESOURCEResourcesCDKORCHCUSTOMRESOURCEOnEventB60C9E76
+        .Properties.Code.S3Key =
+          '3aa61e21c6891872655937328a6920a8bb2146166790237d55e789931f75b60c.zip';
+    }
+    return template;
+  }
+}
+
+/**
+ * Salt value changes every build to the new BUILD_TIME.
+ *
+ * Freeze it to a constant value.
+ */
+export class NormalizeSalt extends StringReplacer {
+  constructor(stack: Stack, id: string = 'NormalizeSalt') {
+    super(stack, id, {
+      splitter: BUILD_TIME.toString(), joiner: '1731294054943',
+    });
+  }
+}
+
+/**
+ * Freeze values we expect to change in snapshots.  That way the snapshots won't change due to
+ * expected changes.
+ */
 export class NormalizeSnapshot extends Construct {
   constructor(stack: Stack, id: string = 'NormalizeSnapshot') {
     super(stack, id);
-    // EnsureChangeInStackB671AB8A and 1731294054943
-    new StringReplacer(stack, 'Replacer', {
-      splitter: BUILD_TIME.toString(), joiner: '1731294054943',
-    });
-    new NormalizeEnsureChangeInStack(stack, 'NormalizeEnsureChangeInStack');
+    new NormalizeCodeS3Key(stack);
+    new NormalizeSalt(stack);
+    new NormalizeEnsureChangeInStack(stack);
   }
 }
