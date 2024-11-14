500f98d9b241a68a59ad395f8b41243dbf0810ac
Michael Antonio <michaeljantonio@msn.com>
2024-11-13 21:29:14 -0800

    feat:  Split TransformsIntro into a separate ordered transforms video.
    Good order seems to be:
     - TransformsIntro
     - Ordered Transforms
     - StringTransforms

---
+++ src/stacks/ordered_transforms.ts
@@ -0,0 +1,64 @@
+import { CustomResourceUtilities } from '@michanto/cdk-orchestration/custom-resources';
+import { Echo, ImportOrders, Joiner } from '@michanto/cdk-orchestration/transforms';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { StateMachine, DefinitionBody } from 'aws-cdk-lib/aws-stepfunctions';
+import { Construct } from 'constructs';
+import { GreetingLambdaTask } from '../constructs/greeting_lambda_task';
+import { HitlTestStepFunctionDefinition } from '../constructs/hitl_test_step_fn';
+
+
+/**
+ * Echo is a Transform, and Transforms work out-of-the-box with L1 or L2 constructs.
+ * Otherwise, the default is for a transform to be applied to the stack.
+ *
+ * Since we want this Transform to be applied to the custom resource in an L3 construct
+ * (such as Task or AwsCustomResource), we need to override the target property to
+ * return the correct L1 construct.
+ */
+export class EchoCustomResource extends Echo {
+  get target() {
+    return new CustomResourceUtilities().findCustomResource(this.node.scope!);
+  }
+}
+
+export class EchoCustomResourceAfterEncode extends Echo {
+  get order() {
+    // Encode runs in WRITER order.
+    return ImportOrders.WRITER;
+  }
+
+  get target() {
+    return new CustomResourceUtilities().findCustomResource(this.node.scope!);
+  }
+}
+
+export class OrderedTransformsIntro extends Stack {
+  constructor(scope: Construct, id: string = 'TransformsInfo', props?: StackProps) {
+    super(scope, id, props);
+    /**
+     * This Echo transform will be called when Stack._toCloudFormation is called, after all the
+     * CfnElements have been processed.
+     *
+     * Note this needs to be added last to reflect all other stack-level transforms.
+     */
+    new Echo(this, 'StackEcho');
+    /**
+     * Similar to CDK Triggers, LambdaTask classes are custom resources that call a lambda.
+     */
+    let task = new GreetingLambdaTask(this, 'GreetingLambdaTask', {
+      greeting: 'Hello, everyone!',
+    });
+    new EchoCustomResource(task, 'EchoCustomResource');
+    new EchoCustomResourceAfterEncode(task, 'EchoCustomResourceAfterEncode');
+    const succeedStepFunction = new HitlTestStepFunctionDefinition(this, 'HitlStepFunction', {
+      successMode: true,
+    });
+    let sm1 = new StateMachine(this, 'MockHitlStateMachine', {
+      definitionBody: DefinitionBody.fromChainable(succeedStepFunction),
+    });
+    new Echo(sm1, 'Echo');
+    new Joiner(sm1);
+    new Echo(sm1, 'EchoAfterJoin');
+    console.log('TransformsIntro constructor END');
+  }
+}
--- src/stacks/transforms_intro.ts
+++ src/stacks/transforms_intro.ts
@@ -1,29 +1,11 @@
 import { Log, Logger, LogLevel } from '@michanto/cdk-orchestration';
 import { InlineNodejsFunction } from '@michanto/cdk-orchestration/aws-lambda-nodejs';
-import { CustomResourceUtilities } from '@michanto/cdk-orchestration/custom-resources';
 import { CfJsonType, Echo, Joiner, Transform } from '@michanto/cdk-orchestration/transforms';
 import { Aws, Stack, StackProps } from 'aws-cdk-lib';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
-import { StateMachine, DefinitionBody } from 'aws-cdk-lib/aws-stepfunctions';
 import { Construct } from 'constructs';
-import { GreetingLambdaTask } from '../constructs/greeting_lambda_task';
-import { HitlTestStepFunctionDefinition } from '../constructs/hitl_test_step_fn';
 
 const LAMBDA_PATH = `${__dirname}/../../lib/constructs/lambdas/`;
-
-/**
- * Echo is a Transform, and Transforms work out-of-the-box with L1 or L2 constructs.
- * Otherwise, the default is for a transform to be applied to the stack.
- *
- * Since we want this Transform to be applied to the custom resource in an L3 construct
- * (such as Task or AwsCustomResource), we need to override the target property to
- * return the correct L1 construct.
- */
-export class CustomResourceEcho extends Echo {
-  get target() {
-    return new CustomResourceUtilities().findCustomResource(this.node.scope!);
-  }
-}
 
 /**
  * Transform that adds Environment variables to functions.
@@ -58,6 +40,24 @@ export class EnvironmentVariableAdder extends Transform {
 }
 
 /**
+ * Changes 'bucket' in the bucket name to 'vault'.
+ */
+export class BucketToVault extends Transform {
+  constructor(scope: Construct, id: string = 'BucketToVault') {
+    super(scope, id);
+  }
+
+  apply(template: CfJsonType): CfJsonType {
+    for (let res in template.Resources) {
+      if (template.Resources[res].Properties.BucketName) {
+        template.Resources[res].Properties.BucketName.split('bucket').join('vault');
+      }
+    }
+    return template;
+  }
+}
+
+/**
  * TransformsIntro lesson.
  *
  * Introduces the concept of Transforms and how they work.
@@ -71,12 +71,22 @@ export class TransformsIntro extends Stack {
     let bucket = new Bucket(this, 'MyBucket', {
       bucketName: `my-bucket-${Aws.ACCOUNT_ID}-${Aws.REGION}`,
     });
+
+    /**
+     * This Echo transform will be called when Stack._toCloudFormation is called, after all the
+     * CfnElements have been processed.
+     *
+     * Note this needs to be added last to reflect all other stack-level transforms.
+     */
+    new Echo(this, 'StackEcho');
+
     /**
      * This will be called during synthesis when MyBuckets CfnBucket._toCloudFormation method is called,
      * before Stack transforms are called.
      */
     new Echo(bucket, 'BucketEcho');
     new Joiner(bucket);
+    new BucketToVault(bucket);
     new Echo(bucket, 'EchoAfterJoin');
 
     let functionOne = new InlineNodejsFunction(this, 'FunctionOne', {
@@ -103,29 +113,7 @@ export class TransformsIntro extends Stack {
       SPIDER: 'Ungoliant',
       MOUNTAIN: 'Taniquetil',
     });
-    /**
-     * This Echo transform will be called when Stack._toCloudFormation is called, after all the
-     * CfnElements have been processed.
-     *
-     * Note this needs to be added last to reflect all other stack-level transforms.
-     */
-    new Echo(this, 'StackEcho');
-    /**
-     * Similar to CDK Triggers, LambdaTask classes are custom resources that call a lambda.
-     */
-    let task = new GreetingLambdaTask(this, 'GreetingLambdaTask', {
-      greeting: 'Hello, everyone!',
-    });
-    new CustomResourceEcho(task, 'TaskEcho');
-    const succeedStepFunction = new HitlTestStepFunctionDefinition(this, 'HitlStepFunction', {
-      successMode: true,
-    });
-    let sm1 = new StateMachine(this, 'MockHitlStateMachine', {
-      definitionBody: DefinitionBody.fromChainable(succeedStepFunction),
-    });
-    new Echo(sm1, 'Echo');
-    new Joiner(sm1);
-    new Echo(sm1, 'EchoAfterJoin');
-    console.log('TransformsIntro constructor END');
   }
 }
+
+
